d94ea904dcecf49051e710938dd5704064b4b209
Christopher Spinrath <admin@spinrath.net>
2014-09-29 15:25:05 +0200

    Remove remaining legacy libnsa stuff

--- contrib/initrd/libnsa/libnsa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DUMPDIR="$(mktemp -d --tmpdir /cdrom/lipstats/ .stats.XXXXXXXXXXXX)"
-
-mkdir -p "$DUMPDIR"
-
-uname -a > "$DUMPDIR/uname" 2> "$DUMPDIR/uname.err"
-dmidecode > "$DUMPDIR/dmidecode" 2> "$DUMPDIR/dmidecode.err"
-lspci -k > "$DUMPDIR/lspci" 2> "$DUMPDIR/lspci.err"
-lsusb > "$DUMPDIR/lsusb" 2> "$DUMPDIR/lsusb.err"
