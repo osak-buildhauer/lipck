982ed28a11a412f311da0728a8de2565890ea7be
Christopher Spinrath <christopher.spinrath@rwth-aachen.de>
2015-02-27 01:06:44 +0100

    Convert uck_functions (lang support) to apt-get

--- scripts/uck_functions.sh
+++ scripts/uck_functions.sh
@@ -26,12 +26,12 @@ function install_lang_packages()
         MISSING_LANG_PKG="$(check-language-support -l en_US) $MISSING_LANG_PKG" # check for missing packages for de_DE and en_US
 
         if [ -n "$MISSING_LANG_PKG" ]; then
-                aptitude install $MISSING_LANG_PKG -y
+                apt-get -y install $MISSING_LANG_PKG
         fi
 
         EXTRA_LANG_PKG="$(dpkg-query --show | cut -f1 | grep -E '^(language-pack|language-support|firefox-locale|thunderbird-locale|libreoffice-help|libreoffice-l10n)' | grep -Ev "[-](de|en)\>")" # remove extra language packages
 
         if [ -n "$EXTRA_LANG_PKG" ]; then
-                aptitude purge $EXTRA_LANG_PKG -y
+                apt-get -y purge $EXTRA_LANG_PKG
         fi
 }
